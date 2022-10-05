9da0290759fe0a322910a188815430459536a805
Philip Basnight <philipbasnight@gmail.com>
2022-10-04 23:39:05 -0400

    winning conditions finished

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ let isClicked = false
 // Event Listeners Here
 
 document.querySelector('section.board').addEventListener('click', click)
-// document.getElementById('sq15').addEventListener('click', click)
-// document.getElementById('sq16').addEventListener('click', click)
-// document.getElementById('sq17').addEventListener('click', click)
-// document.getElementById('sq18').addEventListener('click', click)
-// document.getElementById('sq19').addEventListener('click', click)
+// document.getElementById('sq35').addEventListener('click', click)
+// document.getElementById('sq36').addEventListener('click', click)
+// document.getElementById('sq37').addEventListener('click', click)
+// document.getElementById('sq38').addEventListener('click', click)
+// document.getElementById('sq39').addEventListener('click', click)
+// document.getElementById('sq40').addEventListener('click', click)
+// document.getElementById('sq41').addEventListener('click', click)
 playAgain.addEventListener('click', start)
 ////////////////////////////////
 ////////////////////////////////
@@ -125,29 +127,166 @@ function click(event) {
 function checkWinner() {
   if (winner === false) {
     if (
+      //horizontal
       board[0] + board[1] + board[2] + board[3] === 4 ||
-      board[0] + board[3] + board[6] === 3 ||
-      board[0] + board[4] + board[8] === 3 ||
-      board[1] + board[4] + board[7] === 3 ||
-      board[2] + board[4] + board[6] === 3 ||
-      board[2] + board[5] + board[8] === 3 ||
-      board[3] + board[4] + board[5] === 3 ||
-      board[6] + board[7] + board[8] === 3 ||
-      board[15] + board[16] + board[17] === 3
+      board[1] + board[2] + board[3] + board[4] === 4 ||
+      board[2] + board[3] + board[4] + board[5] === 4 ||
+      board[3] + board[4] + board[5] + board[6] === 4 ||
+      board[7] + board[8] + board[9] + board[10] === 4 ||
+      board[8] + board[9] + board[10] + board[11] === 4 ||
+      board[9] + board[10] + board[11] + board[12] === 4 ||
+      board[10] + board[11] + board[12] + board[13] === 4 ||
+      board[14] + board[15] + board[16] + board[17] === 4 ||
+      board[15] + board[16] + board[17] + board[18] === 4 ||
+      board[16] + board[17] + board[18] + board[19] === 4 ||
+      board[17] + board[18] + board[19] + board[20] === 4 ||
+      board[21] + board[22] + board[23] + board[24] === 4 ||
+      board[22] + board[23] + board[24] + board[25] === 4 ||
+      board[23] + board[24] + board[25] + board[26] === 4 ||
+      board[24] + board[25] + board[26] + board[27] === 4 ||
+      board[28] + board[29] + board[30] + board[31] === 4 ||
+      board[29] + board[30] + board[31] + board[32] === 4 ||
+      board[30] + board[31] + board[32] + board[33] === 4 ||
+      board[31] + board[32] + board[33] + board[34] === 4 ||
+      board[35] + board[36] + board[37] + board[38] === 4 ||
+      board[36] + board[37] + board[38] + board[39] === 4 ||
+      board[37] + board[38] + board[39] + board[40] === 4 ||
+      board[38] + board[39] + board[40] + board[41] === 4 ||
+      ///vertical
+      board[0] + board[7] + board[14] + board[21] === 4 ||
+      board[7] + board[14] + board[21] + board[28] === 4 ||
+      board[14] + board[21] + board[28] + board[35] === 4 ||
+      board[1] + board[8] + board[15] + board[22] === 4 ||
+      board[8] + board[15] + board[22] + board[29] === 4 ||
+      board[15] + board[22] + board[29] + board[36] === 4 ||
+      board[2] + board[9] + board[16] + board[23] === 4 ||
+      board[9] + board[16] + board[23] + board[30] === 4 ||
+      board[16] + board[23] + board[30] + board[37] === 4 ||
+      board[3] + board[10] + board[17] + board[24] === 4 ||
+      board[10] + board[17] + board[24] + board[31] === 4 ||
+      board[17] + board[24] + board[31] + board[38] === 4 ||
+      board[4] + board[11] + board[18] + board[25] === 4 ||
+      board[11] + board[18] + board[25] + board[32] === 4 ||
+      board[18] + board[25] + board[32] + board[38] === 4 ||
+      board[5] + board[12] + board[19] + board[26] === 4 ||
+      board[12] + board[19] + board[26] + board[33] === 4 ||
+      board[19] + board[26] + board[33] + board[39] === 4 ||
+      board[6] + board[13] + board[20] + board[27] === 4 ||
+      board[13] + board[20] + board[27] + board[34] === 4 ||
+      board[20] + board[27] + board[34] + board[40] === 4 ||
+      board[7] + board[14] + board[21] + board[28] === 4 ||
+      board[14] + board[21] + board[28] + board[35] === 4 ||
+      board[21] + board[28] + board[35] + board[41] === 4 ||
+      //diagonal left
+
+      board[3] + board[9] + board[15] + board[21] === 4 ||
+      board[4] + board[10] + board[16] + board[22] === 4 ||
+      board[5] + board[11] + board[17] + board[23] === 4 ||
+      board[6] + board[12] + board[18] + board[24] === 4 ||
+      board[10] + board[16] + board[22] + board[28] === 4 ||
+      board[11] + board[17] + board[23] + board[29] === 4 ||
+      board[12] + board[18] + board[24] + board[30] === 4 ||
+      board[13] + board[19] + board[25] + board[31] === 4 ||
+      board[17] + board[23] + board[29] + board[35] === 4 ||
+      board[18] + board[24] + board[30] + board[36] === 4 ||
+      board[19] + board[25] + board[31] + board[37] === 4 ||
+      board[20] + board[26] + board[32] + board[38] === 4 ||
+      //diagonal right
+      board[3] + board[11] + board[19] + board[27] === 4 ||
+      board[2] + board[10] + board[18] + board[26] === 4 ||
+      board[1] + board[9] + board[17] + board[25] === 4 ||
+      board[0] + board[8] + board[16] + board[24] === 4 ||
+      board[10] + board[18] + board[26] + board[34] === 4 ||
+      board[9] + board[17] + board[25] + board[33] === 4 ||
+      board[8] + board[16] + board[24] + board[32] === 4 ||
+      board[7] + board[15] + board[23] + board[31] === 4 ||
+      board[17] + board[25] + board[33] + board[41] === 4 ||
+      board[16] + board[24] + board[32] + board[40] === 4 ||
+      board[15] + board[23] + board[31] + board[39] === 4 ||
+      board[14] + board[22] + board[30] + board[38] === 4
     ) {
       message.textContent = 'PLAYER 1 WINS!'
       message.style.color = 'rgba(223, 254, 215, 0.913)'
       winner = true
     }
     if (
-      board[0] + board[1] + board[2] === -3 ||
-      board[0] + board[3] + board[6] === -3 ||
-      board[0] + board[4] + board[8] === -3 ||
-      board[1] + board[4] + board[7] === -3 ||
-      board[2] + board[4] + board[6] === -3 ||
-      board[2] + board[5] + board[8] === -3 ||
-      board[3] + board[4] + board[5] === -3 ||
-      board[6] + board[7] + board[8] === -3
+      //horizontal
+      board[0] + board[1] + board[2] + board[3] === -4 ||
+      board[1] + board[2] + board[3] + board[4] === -4 ||
+      board[2] + board[3] + board[4] + board[5] === -4 ||
+      board[3] + board[4] + board[5] + board[6] === -4 ||
+      board[7] + board[8] + board[9] + board[10] === -4 ||
+      board[8] + board[9] + board[10] + board[11] === -4 ||
+      board[9] + board[10] + board[11] + board[12] === -4 ||
+      board[10] + board[11] + board[12] + board[13] === -4 ||
+      board[14] + board[15] + board[16] + board[17] === -4 ||
+      board[15] + board[16] + board[17] + board[18] === -4 ||
+      board[16] + board[17] + board[18] + board[19] === -4 ||
+      board[17] + board[18] + board[19] + board[20] === -4 ||
+      board[21] + board[22] + board[23] + board[24] === -4 ||
+      board[22] + board[23] + board[24] + board[25] === -4 ||
+      board[23] + board[24] + board[25] + board[26] === -4 ||
+      board[24] + board[25] + board[26] + board[27] === -4 ||
+      board[28] + board[29] + board[30] + board[31] === -4 ||
+      board[29] + board[30] + board[31] + board[32] === -4 ||
+      board[30] + board[31] + board[32] + board[33] === -4 ||
+      board[31] + board[32] + board[33] + board[34] === -4 ||
+      board[35] + board[36] + board[37] + board[38] === -4 ||
+      board[36] + board[37] + board[38] + board[39] === -4 ||
+      board[37] + board[38] + board[39] + board[40] === -4 ||
+      board[38] + board[39] + board[40] + board[41] === -4 ||
+      ///vertical
+      board[0] + board[7] + board[14] + board[21] === -4 ||
+      board[7] + board[14] + board[21] + board[28] === -4 ||
+      board[14] + board[21] + board[28] + board[35] === -4 ||
+      board[1] + board[8] + board[15] + board[22] === -4 ||
+      board[8] + board[15] + board[22] + board[29] === -4 ||
+      board[15] + board[22] + board[29] + board[36] === -4 ||
+      board[2] + board[9] + board[16] + board[23] === -4 ||
+      board[9] + board[16] + board[23] + board[30] === -4 ||
+      board[16] + board[23] + board[30] + board[37] === -4 ||
+      board[3] + board[10] + board[17] + board[24] === -4 ||
+      board[10] + board[17] + board[24] + board[31] === -4 ||
+      board[17] + board[24] + board[31] + board[38] === -4 ||
+      board[4] + board[11] + board[18] + board[25] === -4 ||
+      board[11] + board[18] + board[25] + board[32] === -4 ||
+      board[18] + board[25] + board[32] + board[38] === -4 ||
+      board[5] + board[12] + board[19] + board[26] === -4 ||
+      board[12] + board[19] + board[26] + board[33] === -4 ||
+      board[19] + board[26] + board[33] + board[39] === -4 ||
+      board[6] + board[13] + board[20] + board[27] === -4 ||
+      board[13] + board[20] + board[27] + board[34] === -4 ||
+      board[20] + board[27] + board[34] + board[40] === -4 ||
+      board[7] + board[14] + board[21] + board[28] === -4 ||
+      board[14] + board[21] + board[28] + board[35] === -4 ||
+      board[21] + board[28] + board[35] + board[41] === -4 ||
+      //diagonal left
+
+      board[3] + board[9] + board[15] + board[21] === -4 ||
+      board[4] + board[10] + board[16] + board[22] === -4 ||
+      board[5] + board[11] + board[17] + board[23] === -4 ||
+      board[6] + board[12] + board[18] + board[24] === -4 ||
+      board[10] + board[16] + board[22] + board[28] === -4 ||
+      board[11] + board[17] + board[23] + board[29] === -4 ||
+      board[12] + board[18] + board[24] + board[30] === -4 ||
+      board[13] + board[19] + board[25] + board[31] === -4 ||
+      board[17] + board[23] + board[29] + board[35] === -4 ||
+      board[18] + board[24] + board[30] + board[36] === -4 ||
+      board[19] + board[25] + board[31] + board[37] === -4 ||
+      board[20] + board[26] + board[32] + board[38] === -4 ||
+      //diagonal right
+      board[3] + board[11] + board[19] + board[27] === -4 ||
+      board[2] + board[10] + board[18] + board[26] === -4 ||
+      board[1] + board[9] + board[17] + board[25] === -4 ||
+      board[0] + board[8] + board[16] + board[24] === -4 ||
+      board[10] + board[18] + board[26] + board[34] === -4 ||
+      board[9] + board[17] + board[25] + board[33] === -4 ||
+      board[8] + board[16] + board[24] + board[32] === -4 ||
+      board[7] + board[15] + board[23] + board[31] === -4 ||
+      board[17] + board[25] + board[33] + board[41] === -4 ||
+      board[16] + board[24] + board[32] + board[40] === -4 ||
+      board[15] + board[23] + board[31] + board[39] === -4 ||
+      board[14] + board[22] + board[30] + board[38] === -4
     ) {
       message.textContent = 'PLAYER 2 WINS!'
       message.style.color = 'rgba(198, 224, 255, 0.913)'
